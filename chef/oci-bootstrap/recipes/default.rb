7d5ad11e8487fef1e241333ca6fcaacf8b5fd51a
Romil Bhagat <romil@limepoint.com>
2020-04-24 12:10:01 +1000

    moved around sssd restart to be last

--- chef/oci-bootstrap/recipes/default.rb
+++ chef/oci-bootstrap/recipes/default.rb
@@ -82,8 +82,6 @@ end
 # It tries to create the directory which by that point has become a mount, and throws Read-only file system @ apply2files - /oracle/stage
 execute 'mkdir -p /oracle/stage'
 
-
-
 ### --- Set up SSSD For LDAP Authentication --- ###
 # Setup the SSSD Subsystem. This is from the sssd_ldap cookbook
 include_recipe 'sssd_ldap'
@@ -99,13 +97,6 @@ template '/etc/ssh/sshd_config' do
   owner 'root'
   group 'root'
   mode '0600'
-end
-
-# RB: Do this to for a workaround on sudoers not working;
-# RB: there is no guarantee that this fixes it but this has worked all the time
-service 'sssd' do
-  ignore_failure true
-  action :restart
 end
 
 # This is required to create home directories for the LDAP users
@@ -162,6 +153,12 @@ gem_package "cicphash"
 
 # Include recipe for adding VM into the LDAP
 include_recipe '::ldap-client-configs'
+
+# RB: Do this to for a workaround on sudoers not working;
+# RB: there is no guarantee that this fixes it but this has worked all the time
+service 'sssd' do
+  action :restart
+end
 
 # Make the chef-client a system service unless you are running oel6 (for och)
 if node['platform_version'].to_i <= 7
