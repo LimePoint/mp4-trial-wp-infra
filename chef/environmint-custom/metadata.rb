54f93af0bedcc8fcdb2d43a1f99c5d41fdbf71b3
Romil Bhagat <romil@limepoint.com>
2020-04-27 17:32:51 +1000

    rename environmint-custom

--- chef/environmint-custom/metadata.rb
+++ chef/environmint-custom/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'info@limepoint.com'
 license          'ENVIRONMINT(TM) End-User License. Copyright (c) LimePoint Pty Ltd 2014. All rights reserved.'
 description      'Installs/Configures environmint-custom'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.4'
+version '1.0.0'
 
 depends 'obp-environmint-custom'
 depends 'oci-common'
