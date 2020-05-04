c5fb9e36c19532ea5d321ad2494ee135d91616cb
Romil Bhagat <romil@limepoint.com>
2020-05-04 16:49:08 +1000

    added support for node exporter for RHEL 7

---
+++ chef/oci-bootstrap/recipes/setup-node-exporter.rb
@@ -0,0 +1,54 @@
+# This recipe has all configurations required for the LDAP server
+
+group 'node_exporter' do
+  system true
+end
+
+user 'node_exporter' do
+  comment 'node exporter user'
+  gid 'node_exporter'
+  system true
+  shell '/sbin/nologin'
+end
+
+cookbook_file "/etc/sysconfig/node_exporter" do
+  source "sysconfig.node_exporter"
+  mode '0744'
+
+  notifies :run, 'execute[create_directory]', :immediate
+end
+
+cookbook_file "/etc/systemd/system/node_exporter.service" do
+  source "node_exporter.service"
+  mode '0744'
+
+  notifies :run, 'execute[reload_systemd]', :immediate
+  notifies :run, 'execute[copy_binary]', :immediate
+end
+
+execute "create_directory" do
+  command "mkdir -p /opt/node_exporter /var/lib/node_exporter/textfile_collector && chown -R node_exporter:node_exporter /opt/node_exporter /var/lib/node_exporter"
+
+  action :nothing
+end
+
+execute "copy_binary" do
+  command "cp -u /oracle/stage/node_exporter/node_exporter /opt/node_exporter/node_exporter"
+  user 'node_exporter'
+
+  action :nothing
+end
+
+execute "reload_systemd" do
+  command "systemctl daemon-reload"
+
+  action :nothing
+end
+
+service 'node_exporter.service' do
+  action :enable
+end
+
+service 'node_exporter.service' do
+  action :start
+end
