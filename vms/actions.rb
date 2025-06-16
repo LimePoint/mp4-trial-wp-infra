a5549c013c0fdeab4117e13d42705b2f0582b9ec
Romil Bhagat <romil@limepoint.com>
2025-06-16 13:13:29 +1000

    desription for global action

--- vms/actions.rb
+++ vms/actions.rb
@@ -128,5 +128,5 @@ end
 
 # this block will create actions like cix1-create
 %w(create start stop restart exists? destroy).each do |act|
-  action "allvms-#{act}", steps: asset_actions.select { |ha| ha.match?(/#{act}/)}, run_as: :parallel
+  action "allvms-#{act}", steps: asset_actions.select { |ha| ha.match?(/#{act}/)}, run_as: :parallel, description: "allvms-#{act}"
 end
