aa012f6460f71607d8c2c4313a520e469b398546
Romil Bhagat <romil@limepoint.com>
2026-04-17 08:19:52 +1000

    commented out everything

--- vms/utility.rb
+++ vms/utility.rb
@@ -1,11 +1,11 @@
-# utility methods
-def update_security_rules(host_obj, security_rules)
-  ref_security_rules = []
-  security_rules = Array(security_rules)
-  security_rules.each do|security_rule|
-    ref_security_rules << ref(security_rule).controller
-  end
-  puts "updated network_security_groups = #{ref_security_rules}"
-  host_obj.network_security_groups = ref_security_rules
+# # utility methods
+# def update_security_rules(host_obj, security_rules)
+#   ref_security_rules = []
+#   security_rules = Array(security_rules)
+#   security_rules.each do|security_rule|
+#     ref_security_rules << ref(security_rule).controller
+#   end
+#   puts "updated network_security_groups = #{ref_security_rules}"
+#   host_obj.network_security_groups = ref_security_rules
 
-end
+# end
